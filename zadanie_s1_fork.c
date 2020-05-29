1a7b7833cb89f175956d666d95dcd1cf586268e6
artraf <artraf@yandex.ru>
2020-05-29 23:31:22 +0200

    asdad

---
+++ zadanie_s1_fork.c
@@ -0,0 +1,24 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char* argv[])
+{
+	pid_t mojpid, x;
+	mojpid = getpid();
+	printf("[%u]: Run zadanie_s1\n", mojpid);
+	x = fork();
+	if (x == 0)
+	{
+		if (execl("/home/PJWSTK/s21357/SOP_C/sop_git/zadanie_s1", "zadanie_s1", "USER", NULL) == -1)
+		{
+			printf("Uruchomienie nie powiodlo sie\n");
+		}
+	}
+	else
+	{
+		waitpid(x, NULL, 0);
+		printf("[%u]: zadanie_s1 zakonczony\n", mojpid);
+	}
+	return 0;
+}
